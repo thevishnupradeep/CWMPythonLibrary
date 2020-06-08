78b32d56c4aa4651c6007c51d763de2e472bd7db
vishpradeep <vishpradeep@users.noreply.github.com>
2020-06-08 15:24:42 +0400

    Added check for log ID

--- codewatchman/watchmanlog.py
+++ codewatchman/watchmanlog.py
@@ -10,15 +10,22 @@ class WatchmanLog:
         self.log_code = log_code
         self.validate()
 
+
     def validate(self):
         if self.message is None or type(self.message) is not str:
             print("Invalid log data. Message is required.")
             self.is_valid = False
+            return
 
         if self.log_code is None or type(self.log_code) is not str:
             print("Invalid log data. log_code is required and it has to be a string")
             self.is_valid = False
+            return
 
         if self.payload != None and type(self.payload) is not dict:
             print("Invalid log data. Payload has to be a python dictionary.")
             self.is_valid = False
+            return
+
+        self.is_valid = True
+
